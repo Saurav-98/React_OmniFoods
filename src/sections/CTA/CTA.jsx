cd2b6acf00cc67a5589ad4efa600320fe4be6dfb
Saurav Kumar Verma <sauravkumarverma22@gmail.com>
2023-01-25 17:24:46 +0530

    working on cta

---
+++ src/sections/CTA/CTA.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./CTA.css";
+
+const CTA = () => {
+  return (
+    <section className="cta">
+      <div className="container">
+<div className="cta-text-box"></div>
+<div className="cta-img-box"></div>
+      </div>
+    </section>
+  )
+}
+
+export default CTA
